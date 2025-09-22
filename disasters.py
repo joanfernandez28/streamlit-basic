f916140f773f359f8c37f70e3e988ae567a6e9ff
Joan Fernandez Valera <joanfernandezva@gmail.com>
2025-09-22 17:41:13 +0200

    Añadida nueva tabla en disasters.py

---
+++ disasters.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import altair as alt
+import vega_datasets
+
+@st.cache_data
+def load_data():
+    return vega_datasets.data.disasters()
+
+disasters = load_data()
+
+st.write(disasters)
+
+mpg = alt.Chart(disasters).mark_point().encode(
+    alt.X('Year'),
+    alt.Y('Deaths'),
+    color='Entity',
+    tooltip=['Entity', 'Deaths']
+).interactive()
